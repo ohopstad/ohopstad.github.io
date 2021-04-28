4aaef4e3cabf2a6d733c18caefc3807403d85019
olavhop <22353118+ohopstad@users.noreply.github.com>
2021-04-28 19:45:00 +0200

    oops forgot to upload

--- js/themoviedb.js
+++ js/themoviedb.js
@@ -1,32 +1,47 @@
-async function test_inline_external_id(){
-    const URL = "https://api.themoviedb.org/3/find/"
+/*
+fetches 
+*/
+async function test_id(){
+    const URL = "https://api.themoviedb.org/3/";
+    const poster_url = "https://image.tmdb.org/t/p/original";
     let key3 = document.querySelector("#api_v3").value;
-    let key4 = document.querySelector('#api_v4').value;
     
     let service = document.querySelector("#service").value;
-    let ext_id = document.querySelector("#id").value;
-    let fetch_req = fetch(URL + ext_id 
-        + "?api_key=" + key3 
+    let id = document.querySelector("#id").value;
+    let fetch_url = URL;
+    let url_get = "?api_key=" + key3 
         + "&language=" + "en-US"
-        + "&external_source=" + service)
+        + "&external_source=" + service;
+    if (service == "movie_id"){
+        fetch_url += "movie/"  + id + url_get;
+    }
+    else{
+        fetch_url += "find/" + id + url_get;
+    }
+    let fetch_req = fetch(fetch_url)
         .catch((error)=>{
             console.out(error);
             let out = document.querySelector("#api_problems");
             out.innerHTML = error.toString();
             out.style.display = inline;
-            return;
+            return true;
         });
-    let data = await fetch_req;
-    //console.log(data);
+    let data = await fetch_req;  
+    if (!data.ok){
+        let out = document.querySelector("#api_problems");
+        out.innerHTML = data.status.toString();
+        out.style.display = inline;
+        return true;
+    }  
     let jsonData = await data.json(); 
-    //console.log(jsonData);
-    let ret = {movies : [], TV : []};
-    for (movie in jsonData.movie_results){
-        ret.movies.push(movie.title);
+
+    //---results
+    output = document.querySelector("#output");
+    //clear
+    output.innerHTML = "";
+    results = jsonData.movie_results.concat(jsonData.tv_results);
+
+    for (ans of results){
+        output.innerHTML +=  "<br><img src='" + poster_url + ans.poster_path + "' width=100 height=150>";
     }
-    for (show of jsonData.tv_results){
-        ret.TV.push(show.name);
-    }
-    //console.log(ret);
-    document.querySelector("#output").innerHTML = "movies: " + ret.movies.toString() + "<br>TV: " + ret.TV.toString();
 }
