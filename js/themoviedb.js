87fdfca0d43558c2b9935b6970b6122c2bad1e47
olavhop <22353118+ohopstad@users.noreply.github.com>
2021-04-28 22:51:50 +0200

    working search queries

--- js/themoviedb.js
+++ js/themoviedb.js
@@ -1,47 +1,132 @@
-/*
-fetches 
-*/
-async function test_id(){
-    const URL = "https://api.themoviedb.org/3/";
-    const poster_url = "https://image.tmdb.org/t/p/original";
-    let key3 = document.querySelector("#api_v3").value;
-    
-    let service = document.querySelector("#service").value;
+async function search(){
+    const key3 = document.querySelector("#api_v3").value;
     let id = document.querySelector("#id").value;
-    let fetch_url = URL;
-    let url_get = "?api_key=" + key3 
-        + "&language=" + "en-US"
-        + "&external_source=" + service;
-    if (service == "movie_id"){
-        fetch_url += "movie/"  + id + url_get;
+    let language = document.querySelector("#language").value;
+    let type = document.querySelector("#tv-or-movie").value;
+    let json = "";
+    let query = document.querySelector("#query").value;
+
+    if(language == ""){
+        language = "en-US";
+    }
+
+    if (id != ""){
+        let service = document.querySelector("#service").value;
+        if (service != "tmdb"){
+            json = await search_id_external(key3, id, service, language); 
+            write_multiple_results(json);
+            return;
+        }
+        json = await search_id(key3, id, type, language);
+        console.log(json);
+        write_single_result(json);
     }
     else{
-        fetch_url += "find/" + id + url_get;
+        if(query != ""){
+            json = await search_tmdb(key3, type, query);
+            console.log(json);
+            write_multiple_results(json);
+        }
     }
+}
+/**
+ * 
+ * @param {*} error 
+ */
+function print_issues(error){
+    let container = document.querySelector("#api_problems");
+    container.innerHTML = error.toString();
+    container.style.display = "block";
+} 
+
+/**
+ * 
+ * @param {*} api_key 
+ * @param {*} id 
+ * @param {*} language 
+ */
+async function search_id(api_key, id, type, language){
+    const URL = "https://api.themoviedb.org/3/"
+    let fetch_url = URL + type + "/" + id;
+    let url_get = "?api_key=" + api_key
+                + "&language=" + language;
+    fetch_url += url_get;
     let fetch_req = fetch(fetch_url)
         .catch((error)=>{
-            console.out(error);
-            let out = document.querySelector("#api_problems");
-            out.innerHTML = error.toString();
-            out.style.display = inline;
-            return true;
+            console.err(error);
+            print_issues(error);
+        });
+    let data = await fetch_req;
+    if (!data.ok){
+        print_issues(data.statusText.toString());
+    }
+    let jsonData = await data.json();
+    return jsonData;
+}
+async function search_tmdb(api_key, type, query){
+    const URL = "https://api.themoviedb.org/3/search/"
+    let fetch_url = URL + type + "/";
+    let url_get = "?api_key" + api_key
+                + "&language=" + language
+                + "&query=" + query;
+                fetch_url += url_get;
+    let fetch_req = fetch(fetch_url)
+        .catch((error)=>{
+            console.err(error);
+            print_issues(error);
+        });
+    let data = await fetch_req;
+    if (!data.ok){
+        print_issues(data.statusText.toString());
+    }
+    let jsonData = await data.json();
+    return jsonData;
+}
+/**
+ * 
+ * @returns 
+ */
+async function search_id_external(api_key, id, service, language){
+    const URL = "https://api.themoviedb.org/3/";
+    
+    let fetch_url = URL;
+    let url_get = "?api_key=" + api_key //api-key
+                + "&language=" + language
+                + "&external_source=" + service + "_id";
+
+    fetch_url += "find/" + id + url_get;
+    let fetch_req = fetch(fetch_url)
+        .catch((error)=>{
+            print_issues(error);
         });
     let data = await fetch_req;  
     if (!data.ok){
-        let out = document.querySelector("#api_problems");
-        out.innerHTML = data.status.toString();
-        out.style.display = inline;
-        return true;
+        print_issues(data.statusText.toString());
     }  
     let jsonData = await data.json(); 
-
-    //---results
-    output = document.querySelector("#output");
-    //clear
+    return jsonData;
+}
+async function write_single_result(results_promise){
+    let container = document.querySelector("#output");
+    add_poster(results_promise.poster_path, container)
+}
+/**
+ * 
+ * @param {*} results_json 
+ */
+async function write_multiple_results(results_promise){
+    const poster_url = "https://image.tmdb.org/t/p/original";
+    let results_json = await results_promise; 
+    let container = document.querySelector("#output");
     output.innerHTML = "";
-    results = jsonData.movie_results.concat(jsonData.tv_results);
 
+    let results = results_json.movie_results.concat(results_json.tv_results);
     for (ans of results){
-        output.innerHTML +=  "<br><img src='" + poster_url + ans.poster_path + "' width=100 height=150>";
+        add_poster(ans.poster_path, container);
     }
+
+}
+function add_poster(path, container){
+    const poster_url = "https://image.tmdb.org/t/p/original";
+    container.innerHTML +=  "<img src='" + poster_url + path + "' width=100 height=150>";
 }
